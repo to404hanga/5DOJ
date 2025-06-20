066a58a298d781bfa79512bd94ac1cd64fa803b1
LinSrHang <1023948565@qq.com>
2025-06-21 01:05:36 +0800

    judger模块新增Delete接口声明

--- backend/judger/internal/service/types.go
+++ backend/judger/internal/service/types.go
@@ -10,4 +10,5 @@ type IJudgerService interface {
 	Compile(ctx context.Context, compiler domain.Compiler, filename string, content string) (fileId string, err error)
 	Run(ctx context.Context, fileId string, input string, timeLimit, memoryLimit int) (output string, err error)
 	Judge(ctx context.Context, fileId string, programId string) (err error)
+	Delete(ctx context.Context, fileId string) (err error)
 }

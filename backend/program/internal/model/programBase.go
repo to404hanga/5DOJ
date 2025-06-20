0e72ab4b08eb4856b0a5fa794acc554ecc87d11e
LinSrHang <1023948565@qq.com>
2025-06-21 03:18:36 +0800

    program模块题目基础信息模型:新增字段:时间限制、内存限制

--- backend/program/internal/model/programBase.go
+++ backend/program/internal/model/programBase.go
@@ -8,6 +8,8 @@ type ProgramBase struct {
 	Level       int8      `gorm:"column:level;type:tinyint;index:idx_level;type:int;not null;comment:题目等级"` // 题目等级
 	Tags        string    `gorm:"column:tags;type:varchar(255);default '';comment:题目标签，按英文逗号分隔"`            // 题目标签，按英文逗号分隔
 	TestCaseNum int8      `gorm:"column:test_case_num;type:tinyint;not null;comment:测试用例数量"`                // 测试用例数量
+	TimeLimit   uint64    `gorm:"column:time_limit;type:bigint unsigned;not null;comment:时间限制，单位 ns"`       // 时间限制，单位 ns
+	MemoryLimit uint64    `gorm:"column:memory_limit;type:bigint unsigned;not null;comment:内存限制，单位 b"`      // 内存限制，单位 b
 	CreatedBy   uint64    `gorm:"column:created_by;type:bigint unsigned;not null;comment:创建者 id"`           // 创建者 id
 	UpdatedBy   uint64    `gorm:"column:updated_by;type:bigint unsigned;not null;comment:更新者 id"`           // 更新者 id
 	CreatedAt   time.Time `gorm:"column:created_at;not null;comment:创建时间"`                                  // 创建时间

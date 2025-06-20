cd0a3c50931901dc08c3f16ec65ac11ea29c26bf
LinSrHang <1023948565@qq.com>
2025-06-21 01:01:47 +0800

    judger模块接口声明

---
+++ backend/judger/internal/service/types.go
@@ -0,0 +1,13 @@
+package service
+
+import (
+	"context"
+
+	"github.com/to404hanga/5DOJ/judger/internal/domain"
+)
+
+type IJudgerService interface {
+	Compile(ctx context.Context, compiler domain.Compiler, filename string, content string) (fileId string, err error)
+	Run(ctx context.Context, fileId string, input string, timeLimit, memoryLimit int) (output string, err error)
+	Judge(ctx context.Context, fileId string, programId string) (err error)
+}

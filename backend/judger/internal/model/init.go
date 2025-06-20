f2e17b3a2ffda58e85b757d6ab8213dd9a2e2a49
LinSrHang <1023948565@qq.com>
2025-06-21 02:35:08 +0800

    judger模块模型初始化

---
+++ backend/judger/internal/model/init.go
@@ -0,0 +1,34 @@
+package model
+
+import (
+	"context"
+	"time"
+
+	"github.com/to404hanga/5DOJ/judger/internal/global"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func InitMySQL() error {
+	return global.SqlDB.AutoMigrate(
+		&ProgramBase{},
+	)
+}
+
+func InitMongoDB() error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	col := global.MongoDB.Collection("program")
+	if _, err := col.Indexes().CreateMany(ctx, []mongo.IndexModel{
+		{
+			Keys:    bson.M{"program_id": 1},
+			Options: options.Index().SetUnique(true),
+		},
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}

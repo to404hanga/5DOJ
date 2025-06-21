c8b87cbcf05fad0e8c43c1af448941a1482f9444
LinSrHang <1023948565@qq.com>
2025-06-22 01:05:57 +0800

    program模块serive接口声明

--- backend/program/internal/service/types.go
+++ backend/program/internal/service/types.go
@@ -1,4 +1,19 @@
 package service
 
+import (
+	"context"
+
+	"github.com/to404hanga/5DOJ/pkg/constant"
+	"github.com/to404hanga/5DOJ/program/internal/domain"
+)
+
 type IProgramService interface {
+	CreateProgram(ctx context.Context, title, content string, createdBy uint64, level constant.ProgramLevelType, timeLimit, memoryLimit uint64, tags []string) (programId uint64, err error)
+	AppendProgramTestCase(ctx context.Context, programId, appendedBy uint64, input, expected string) (testCaseNum int, testCaseId string, err error)
+	UpdateTestCaseByProgramIdAndTestCaseId(ctx context.Context, programId uint64, testCaseId, intput, expected string, updatedBy uint64) (err error)
+	DeleteTestCaseByProgramIdAndTestCaseId(ctx context.Context, programId uint64, testCaseId string, deletedBy uint64) (err error)
+	GetProgramByProgramId(ctx context.Context, programId uint64) (program domain.ProgramView, err error)
+	GetTestCasesByProgramId(ctx context.Context, programId uint64) (testCases []domain.TestCaseView, err error)
+	EnableProgram(ctx context.Context, programId uint64, enabledBy uint64) (err error)
+	DisableProgram(ctx context.Context, programId uint64, disabledBy uint64) (err error)
 }

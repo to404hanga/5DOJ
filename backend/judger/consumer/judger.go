e0aba0a1a4a7615aaa26d5fc15155d21ae259e7d
LinSrHang <1023948565@qq.com>
2025-06-23 23:10:31 +0800

    judger微服务consumer层:判题消费者

---
+++ backend/judger/consumer/judger.go
@@ -0,0 +1,50 @@
+package consumer
+
+import (
+	"5DOJ/judger/global"
+	"5DOJ/judger/service"
+	"5DOJ/pkg/constant/contestMode"
+	"5DOJ/pkg/constant/language"
+	"5DOJ/pkg/constant/topic"
+	"context"
+
+	"github.com/IBM/sarama"
+	"github.com/to404hanga/pkg404/saramax"
+)
+
+type JudgerSubmitConsumer struct {
+	svc service.IJudgerService
+}
+
+var _ Consumer = (*JudgerSubmitConsumer)(nil)
+
+func NewJudgerSubmitConsumer(svc service.IJudgerService) *JudgerSubmitConsumer {
+	return &JudgerSubmitConsumer{
+		svc: svc,
+	}
+}
+
+func (j *JudgerSubmitConsumer) Start() error {
+	cg, err := sarama.NewConsumerGroupFromClient(topic.TopicSubmitEvent, global.Kafka)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		err = cg.Consume(context.Background(), []string{topic.TopicSubmitEvent}, saramax.NewHandler[topic.SubmitEvent](global.L, j.Consume))
+		if err != nil {
+			// TODO: 记录日志
+			panic(err)
+		}
+	}()
+
+	return nil
+}
+
+func (j *JudgerSubmitConsumer) Consume(msg *sarama.ConsumerMessage, evt topic.SubmitEvent) (err error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	_, _, _, err = j.svc.Judge(ctx, evt.RecordId, evt.ProblemId, language.LanguageType(evt.Language), evt.Code, contestMode.ContestModeType(evt.Mode))
+	return
+}

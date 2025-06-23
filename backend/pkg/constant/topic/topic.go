f6262a144f124ac09f92d28e87c6605abb0038c7
LinSrHang <1023948565@qq.com>
2025-06-24 02:17:09 +0800

    SubmitEvent模型添加FilenameWithoutExt字段

--- backend/pkg/constant/topic/topic.go
+++ backend/pkg/constant/topic/topic.go
@@ -3,11 +3,12 @@ package topic
 const TopicSubmitEvent = "submit_event"
 
 type SubmitEvent struct {
-	RecordId  uint64 `json:"recordId"`
-	ContestId uint64 `json:"contestId"`
-	ProblemId uint64 `json:"problemId"`
-	UserId    uint64 `json:"userId"`
-	Code      string `json:"code"`
-	Language  string `json:"language"`
-	Mode      string `json:"mode"`
+	RecordId           uint64 `json:"recordId"`
+	ContestId          uint64 `json:"contestId"`
+	ProblemId          uint64 `json:"problemId"`
+	UserId             uint64 `json:"userId"`
+	FilenameWithoutExt string `json:"filenameWithoutExt"`
+	Code               string `json:"code"`
+	Language           string `json:"language"`
+	Mode               string `json:"mode"`
 }

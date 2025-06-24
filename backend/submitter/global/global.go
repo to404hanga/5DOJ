d285fe8ea9f2b3885463a71ccdb780edd05048fb
LinSrHang <1023948565@qq.com>
2025-06-24 15:15:58 +0800

    submitter微服务global层:新增日志、etcd、grpc server全局变量

--- backend/submitter/global/global.go
+++ backend/submitter/global/global.go
@@ -2,12 +2,18 @@ package global
 
 import (
 	"github.com/IBM/sarama"
+	"github.com/to404hanga/pkg404/grpcx"
+	"github.com/to404hanga/pkg404/logger"
+	clientv3 "go.etcd.io/etcd/client/v3"
 	"go.mongodb.org/mongo-driver/mongo"
 	"gorm.io/gorm"
 )
 
 var (
-	MySQL   *gorm.DB
-	MongoDB *mongo.Database
-	Kafka   sarama.Client
+	MySQL      *gorm.DB
+	MongoDB    *mongo.Database
+	Kafka      sarama.Client
+	L          logger.Logger
+	Etcd       *clientv3.Client
+	GrpcServer *grpcx.Server
 )

256ae7943d9d1b491670cf81d6aaaa4ee803d64a
to404hanga <1023948565@qq.com>
2025-06-29 23:05:13 +0800

    修改ProblemInfo.Enabled字段，默认为true

--- backend/problem/model/problemInfo.go
+++ backend/problem/model/problemInfo.go
@@ -8,7 +8,7 @@ type ProblemInfo struct {
 	Level         int8   `gorm:"type:tinyint;not null;index:idx_level;comment:'题目难度等级: 0=未定义, 1=简单, 2=中等, 3=困难'"` // 题目难度等级: 0=未定义, 1=简单, 2=中等, 3=困难
 	CreatedBy     uint64 `gorm:"type:bigint unsigned;not null;comment:'创建者 ID'"`                                  // 创建者 ID
 	UpdatedBy     uint64 `gorm:"type:bigint unsigned;not null;comment:'最后更新者 ID'"`                                // 最后更新者 ID
-	Enabled       bool   `gorm:"type:tinyint;not null;default:0;comment:'是否启用: 0=禁用, 1=启用'"`                      // 是否启用: 0=禁用, 1=启用
+	Enabled       bool   `gorm:"type:tinyint;not null;default:1;comment:'是否启用: 0=禁用, 1=启用'"`                      // 是否启用: 0=禁用, 1=启用
 	TimeLimit     int    `gorm:"type:int;not null;default:1000;comment:'时间限制: 单位 ms'"`                            // 时间限制: 单位 ms
 	MemoryLimit   int    `gorm:"type:int;not null;default:256;comment:'内存限制: 单位 MB'"`                             // 内存限制: 单位 MB
 	TotalScore    int    `gorm:"type:int;not null;default:0;comment:'总分数（仅 IOI 赛制有效）'"`                           // 总分数（仅 IOI 赛制有效）

939d5c3e2c58f919eb0e9fbdb1ddda8bb6f0a3ed
to404hanga <1023948565@qq.com>
2025-06-29 21:56:45 +0800

    problem微服务domain层:新增ProblemView.TotalTestCase字段，表示启用的用例数

--- backend/problem/domain/problem.go
+++ backend/problem/domain/problem.go
@@ -3,16 +3,17 @@ package domain
 import "time"
 
 type ProblemView struct {
-	Id          uint64
-	Title       string
-	Level       int8
-	CreatedBy   uint64
-	UpdatedBy   uint64
-	Enabled     bool
-	TimeLimit   int
-	MemoryLimit int
-	TotalScore  int
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	Markdown    string
+	Id            uint64
+	Title         string
+	Level         int8
+	CreatedBy     uint64
+	UpdatedBy     uint64
+	Enabled       bool
+	TimeLimit     int
+	MemoryLimit   int
+	TotalScore    int
+	TotalTestCase int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Markdown      string
 }

f3e00196589bd824bc36c0c0e0a7317f82290d18
to404hanga <1023948565@qq.com>
2025-06-29 21:59:30 +0800

    problem微服务model层:新增TestCase.TableName()方法，用于获取表名

--- backend/problem/model/testCase.go
+++ backend/problem/model/testCase.go
@@ -1,14 +1,16 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type TestCase struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	Pid       uint64             `bson:"pid"`
-	Input     string             `bson:"input"`
-	Output    string             `bson:"output"`
-	Score     int                `bson:"score"`
-	CreatedBy uint64             `bson:"createdBy"`
-	UpdatedBy uint64             `bson:"updatedBy"`
-	Enabled   bool               `bson:"enabled"`
+	Tid       string `bson:"tid"`
+	Pid       uint64 `bson:"pid"`
+	Input     string `bson:"input"`
+	Output    string `bson:"output"`
+	Score     int    `bson:"score"`
+	CreatedBy uint64 `bson:"createdBy"`
+	UpdatedBy uint64 `bson:"updatedBy"`
+	Enabled   bool   `bson:"enabled"`
+}
+
+func (TestCase) TableName() string {
+	return "test_case"
 }
